1be9aee13b8a9d4378eaf4812a666eb163037170
alisson <alisson@Alissons-MacBook-Pro.local>
2023-08-09 12:10:32 -0300

    Add postgresql

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module alissonnbarbosaa/shylockgo
 go 1.20
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/gophercloud/gophercloud v1.5.0
-	github.com/mattn/go-sqlite3 v1.14.17
+	github.com/lib/pq v1.10.9
 )
--- main.go
+++ main.go
@@ -8,53 +8,54 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/servers"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
 )
 
-func main()  {
-  logFile, err := os.OpenFile(os.Getenv("LOGFILE"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-  if err != nil {
-    fmt.Println("[ERROR] Error creating log file:", err)
-    os.Exit(1)
-  }
-  defer logFile.Close()
+func main() {
+	logFile, err := os.OpenFile(os.Getenv("LOGFILE"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("[ERROR] Error creating log file:", err)
+		os.Exit(1)
+	}
+	defer logFile.Close()
 
-  log.SetOutput(logFile)
+	log.SetOutput(logFile)
 
-  opts, err := openstack.AuthOptionsFromEnv()
-  if err != nil {
-    log.Println("[ERROR] Error reading environment variables:", err)
-    os.Exit(1)
-  }
+	opts, err := openstack.AuthOptionsFromEnv()
+	if err != nil {
+		log.Println("[ERROR] Error reading environment variables:", err)
+		os.Exit(1)
+	}
 
-  provider, err := openstack.AuthenticatedClient(opts)
-  if err != nil {
-    log.Println("[ERROR] Error creating authenticated client", err)
-    os.Exit(1)
-  }
+	provider, err := openstack.AuthenticatedClient(opts)
+	if err != nil {
+		log.Println("[ERROR] Error creating authenticated client", err)
+		os.Exit(1)
+	}
 
-  client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
-    Region: os.Getenv("OS_REGION_NAME"),
-  })
-  if err != nil {
-    log.Println("[ERROR] Error creating ComputeV2 client:", err)
-    os.Exit(1)
-  }
-  
-  for {
-    db, err := connectToDatabase()
-    if err != nil {
-      log.Println("[ERROR] Error connecting to database", err)
-      os.Exit(1)
-    }
-    defer db.Close()
+	client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
+		Region: os.Getenv("OS_REGION_NAME"),
+	})
+	if err != nil {
+		log.Println("[ERROR] Error creating ComputeV2 client:", err)
+		os.Exit(1)
+	}
 
-    _, err = db.Exec(`
+	for {
+		db, err := connectToDatabase()
+		if err != nil {
+			log.Println("[ERROR] Error connecting to database", err)
+			os.Exit(1)
+		}
+		defer db.Close()
+
+		_, err = db.Exec(`
       CREATE TABLE IF NOT EXISTS instances (
-        id INTEGER NOT NULL PRIMARY KEY,
+        id TEXT NOT NULL PRIMARY KEY,
         timestamp TEXT,
         instanceid TEXT,
         name TEXT,
@@ -66,50 +67,58 @@ func main()  {
         userid TEXT
       )
     `)
-    log.Println("[INFO] Saving instances on database")
-    saveInstancesToDatabase(client, db)
-    timer, err := strconv.Atoi(os.Getenv("TIMER"))
-    if err != nil {
-      log.Println("[ERROR] Error converting timer to integer", err)
-      os.Exit(1)
-    }
-    time.Sleep(time.Duration(timer) * time.Minute)
-  }
+		log.Println("[INFO] Saving instances on database")
+		saveInstancesToDatabase(client, db)
+		timer, err := strconv.Atoi(os.Getenv("TIMER"))
+		if err != nil {
+			log.Println("[ERROR] Error converting timer to integer", err)
+			os.Exit(1)
+		}
+		time.Sleep(time.Duration(timer) * time.Minute)
+	}
 }
 
 func connectToDatabase() (*sql.DB, error) {
-  db, err := sql.Open("sqlite3", os.Getenv("DATABASEFILE"))
-  if err != nil {
-    return nil, err
-  }
-  return db, nil
+	dbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		os.Getenv("DBHOST"), 5432, os.Getenv("DBUSER"), os.Getenv("DBPASSWORD"), os.Getenv("DBNAME"))
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 func saveInstancesToDatabase(client *gophercloud.ServiceClient, db *sql.DB) {
-  listOpts := servers.ListOpts{
+	listOpts := servers.ListOpts{
 		AllTenants: true,
 	}
 
-  rows, err := servers.List(client, listOpts).AllPages()
-  if err != nil {
-    log.Println("[ERROR] Error listing instances:", err)
-    return
-  }
-  
-  instanceList, err := servers.ExtractServers(rows)
+	rows, err := servers.List(client, listOpts).AllPages()
+	if err != nil {
+		log.Println("[ERROR] Error listing instances:", err)
+		return
+	}
 
-  if err != nil {
-    log.Println("[ERROR] Error extracting instance list:", err)
-    return
-  }
+	instanceList, err := servers.ExtractServers(rows)
 
-  timestamp := time.Now()
-  for _, instance := range instanceList {
-    
-    _, err := db.Exec("INSERT INTO instances (timestamp, instanceid, name, status, createdat, flavorid, tenantid, hostid, userid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)",
-      timestamp, instance.ID, instance.Name, instance.Status, instance.Created, instance.Flavor["id"], instance.TenantID, instance.HostID, instance.UserID)
-    if err != nil {
-      log.Println("[ERROR] Error inserting instance data:", err)
-    }
-  }
+	if err != nil {
+		log.Println("[ERROR] Error extracting instance list:", err)
+		return
+	}
+
+	timestamp := time.Now()
+	for _, instance := range instanceList {
+
+		_, err := db.Exec("INSERT INTO instances (id, timestamp, instanceid, name, status, createdat, flavorid, tenantid, hostid, userid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+			uuid.New(), timestamp, instance.ID, instance.Name, instance.Status, instance.Created, instance.Flavor["id"], instance.TenantID, instance.HostID, instance.UserID)
+		if err != nil {
+			log.Println("[ERROR] Error inserting instance data:", err)
+		}
+	}
 }
